d2d04a88dc11ad1ff45a6448aa49a12416451efc
Charles Duan <cduan@sbf5.com>
2025-11-14 17:25:51 -0500

    Added beeper

--- lockctl.py
+++ lockctl.py
@@ -6,17 +6,20 @@ import time
 import yaml
 import event_loop
 
+from gpiod.line import Value
+
 with open('config.yaml', 'r') as io:
     config = yaml.safe_load(io)
 
 in_line = config['in_line']
 out_line = config['out_line']
+piezo_line = config['piezo_line']
 
 out_ls = gpiod.LineSettings(
         direction = gpiod.line.Direction.OUTPUT,
         active_low = False,
         drive = gpiod.line.Drive.PUSH_PULL,
-        output_value = gpiod.line.Value.INACTIVE)
+        output_value = Value.INACTIVE)
 
 in_ls = gpiod.LineSettings(
         direction = gpiod.line.Direction.INPUT,
@@ -29,17 +32,24 @@ in_ls = gpiod.LineSettings(
 
 with gpiod.Chip(config['chip']) as chip:
     with chip.request_lines(
-            { in_line: in_ls, out_line: out_ls },
+            { in_line: in_ls, out_line: out_ls, piezo_line: out_ls },
             consumer = config["consumer"]
             ) as req:
+
+        def beep():
+            req.set_value(piezo_line, Value.ACTIVE)
+            event_loop.TimedEvent(
+                    lambda x: req.set_value(piezo_line, Value.INACTIVE),
+                    0.2)
 
         def read_fn(obj):
             print("Read event")
             for e in req.read_edge_events():
+                beep()
                 if e.event_type == gpiod.EdgeEvent.Type.FALLING_EDGE:
-                    req.set_value(out_line, gpiod.line.Value.INACTIVE)
+                    req.set_value(out_line, Value.INACTIVE)
                 else:
-                    req.set_value(out_line, gpiod.line.Value.ACTIVE)
+                    req.set_value(out_line, Value.ACTIVE)
 
         event_loop.FDReader(req.fd, read_fn)
 
