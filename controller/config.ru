2cf841721abe01e79606fefe1441ca69721682cc
Charles Duan <cduan@sbf5.com>
2026-06-11 08:48:45 -0400

    Working on management app

--- controller/config.ru
+++ controller/config.ru
@@ -2,9 +2,19 @@ require 'yaml'
 require_relative 'door_app'
 require 'rack/session'
 require 'rack/protection'
+require 'rack/reloader'
+require 'webauthn'
+
+app_config = YAML.load_file('config.yaml')
+
+WebAuthn.configure do |config|
+  # config.allowed_origins = app_config['allowed_origins']
+  config.rp_name = app_config['name']
+end
 
 use Rack::Session::Pool, :expire_after => 60 * 60
 use Rack::Protection
+use Rack::Reloader
 
-DoorApp.opts[:config] = YAML.load_file('config.yaml')
-run DoorApp.freeze.app
+DoorApp.opts[:config] = app_config
+run DoorApp.app
--- controller/door_app.rb
+++ controller/door_app.rb
@@ -4,6 +4,7 @@ require 'yaml'
 require 'socket'
 require 'roda'
 require 'tilt'
+require 'webauthn'
 
 #
 # Manages connection to a door system. This class should be instantiated for
@@ -70,25 +71,108 @@ class DoorApp < Roda
 
   route do |r|
 
+    @session = env['rack.session']
+
     r.root do
-      render "index", locals: { name: env['rack.session'][:name] }
+      view "index", locals: { name: @session[:name] }
     end
 
     r.on 'status' do
-      res = {}
-      opts[:config]['doors'].each do |door|
-        begin
-          res[door['name']] = DoorManager.open(door) { |dm| dm.query('status') }
-        rescue
-          res[door['name']] = "Error: #$!"
-        end
-      end
-      render 'status', locals: { res: res }
+      display_status
     end
 
     r.on 'name', String do |str|
-      env['rack.session'][:name] = str
+      @session[:name] = str
       render 'name', locals: { name: str }
     end
+
+    r.on 'register' do
+      route_register(r)
+    end
+
   end
+
+  def display_status
+    res = {}
+    opts[:config]['doors'].each do |door|
+      begin
+        res[door['name']] = DoorManager.open(door) { |dm| dm.query('status') }
+      rescue
+        res[door['name']] = "Error: #$!"
+      end
+    end
+    view 'status', locals: { res: res }
+  end
+
+  def route_register(r)
+
+    r.is do
+      r.get do
+        show_register_forms(r)
+      end
+
+      r.post do
+        cred = WebAuthn::Credential.from_create(
+          r.params['publicKeyCredential']
+        )
+        begin
+          cred.verify(@session[:creation_challenge])
+          @session.delete(:creation_challenge)
+          @session[:webauthn_create] = {
+            :id => cred.id,
+            :key => cred.public_key,
+            :count => cred.sign_count,
+          }
+          r.redirect "/register/success"
+        rescue WebAuthn::Error => e
+          # Handle verification error
+        end
+      end
+    end
+
+    r.is('name') do
+      r.get do
+      end
+
+      r.post do
+      end
+
+    end
+
+    r.is 'success' do
+      render(
+        'register-success', locals: {
+          webauthn_create: @session[:webauthn_create]
+        }
+      )
+    end
+  end
+
+  def show_register_forms(r)
+    name = r.params['name']
+    if name && name.is_a?(String) && !name.empty?
+      user_id = WebAuthn.generate_user_id
+      options = WebAuthn::Credential.options_for_create(
+        user: {
+          id: user_id,
+          name: name.gsub(/\W+/, ' ').strip.gsub(/\W/, '-'),
+          display_name: name,
+        },
+        authenticator_selection: {
+          residentKey: 'required',
+          userVerification: 'discouraged',
+        },
+      )
+
+      @session[:creation_challenge] = options.challenge
+      view(
+        'register-challenge',
+        locals: { options: options.as_json.to_json, name: name },
+      )
+    else
+      view 'register'
+    end
+  end
+
+
 end
